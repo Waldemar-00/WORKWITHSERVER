224cd576a46204ef697da7968a9b48b9c2dd23b7
uladzimir <vova108vova@gmail.com>
2023-04-23 16:01:20 +0300

    axios request, GET

---
+++ js/axios.js
@@ -0,0 +1,35 @@
+async function getResource (url) {
+  const response = await axios(url) // ! 1
+  if (response.status !== 200) { // ! 2
+    throw new Error(`Could not fetch from ${url}, status: ${response.status}`)
+  }
+  return response //! 3
+}
+function createFromServer (e, data) {
+  e.target.remove()
+  data.forEach(item => {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    let icon
+    if (item.sex === 'male') {
+      icon = 'icons/mars.png'
+    } else {
+      icon = 'icons/female.png'
+    }
+    card.innerHTML = `
+      <img src=${item.photo} alt='person photo'>
+      <div class='name'>${item.name} ${item.surname}</div>
+      <div class='sex'>
+      <img src= ${icon} alt='mail'>
+      </div>
+      <div class='age'>${item.age}</div>
+      `
+    document.querySelector('.app').append(card)
+  })
+}
+getResource('http://localhost:3000/people')
+  .then(data => {
+    document.querySelector('button').addEventListener('click', (e) => {
+      createFromServer(e, data.data)
+    })
+  })
