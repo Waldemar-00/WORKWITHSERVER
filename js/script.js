cd7ec789f36ac6a2dccc79f948c8a3e5a25d75a3
uladzimir <vova108vova@gmail.com>
2023-04-23 13:15:34 +0300

    work with server, XMLHttpRequest, GET

--- js/script.js
+++ js/script.js
@@ -7,7 +7,30 @@ window.addEventListener('DOMContentLoaded', () => {
     //! LOADING - stage - 3
     req.addEventListener('load', () => { //! stage - 4, readystatechange and req.readyState === 4 - for all stages
       if (req.status === 200) {
-        console.log(JSON.parse(req.response))
+        const data = JSON.parse(req.response)
+        console.log(data)
+        document.querySelector('button').addEventListener('click', (e) => {
+          e.target.remove()
+          data.forEach(item => {
+            const card = document.createElement('div')
+            card.classList.add('card')
+            let icon;
+            if (item.sex === 'male') {
+              icon = 'icons/mars.png'
+            } else {
+              icon = 'icons/female.png'
+            }
+            card.innerHTML = `
+              <img src=${item.photo} alt='person photo'>
+              <div class='name'>${item.name} ${item.surname}</div>
+              <div class='sex'>
+              <img src= ${icon} alt='mail'>
+              </div>
+              <div class='age'>${item.age}</div>
+              `
+            document.querySelector('.app').append(card)
+          })
+        }, {once: true})
       } else {
         console.error(req.status)
       }
